9130265ebe09a8187b7cd29c1efd842c54978d6d
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-03-24 20:20:44 +0000

    PP-8: add pigeon maps library, create overview map and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Home from './pages/Home';
+
 import './App.css';
 
 const App = () => {
   return (
     <>
       <h3 className='text-3xl font-bold underline'>Power Plants</h3>
+      <Home />
     </>
   );
 };
--- src/services/powerPlantsServices.tsx
+++ src/services/powerPlantsServices.tsx
@@ -2,10 +2,27 @@ import {powerPlantsBackendInstance} from './instances';
 
 export const getPowerPlantsData = async () => {
   try {
-    const {data, status} = await powerPlantsBackendInstance.get('get-all-plants');
+    const {data, status} = await powerPlantsBackendInstance.get('all-plants');
     return data;
   } catch (error) {
-    console.error('Could not fetch the caontries.', error);
+    console.error('Could not fetch the plants data.', error);
+    return [];
+  }
+};
+
+export const getCountryPlants = async (country: string) => {
+  const params = {
+    country: country,
+  };
+  try {
+    const {data, status} = await powerPlantsBackendInstance.get('country-plants', {
+      params: {
+        country: country,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error('Could not fetch plants details for the selected country.', error);
     return [];
   }
 };
