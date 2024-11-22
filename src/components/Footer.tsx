9aca67e0e43dba4d82d2c917ba51ff0e617564bf
Jarek Polowy <jarekpolowy@gmail.com>
2024-11-22 22:24:46 +0000

    PP-20: add data sources in Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Attribution from './Attribution';
+
+const Footer = () => {
+  return (
+    <>
+      <div className='test'>
+        <Attribution />
+      </div>
+    </>
+  );
+};
+
+export default Footer;
